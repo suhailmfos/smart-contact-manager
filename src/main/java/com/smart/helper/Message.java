bac2b0c959d8532af2d06f3e5d980dacf6336cc3
suhailmfos <suhailmfos@gmail.com>
2024-03-14 16:09:31 +0530

    Smart Contact Manager Project

---
+++ src/main/java/com/smart/helper/Message.java
@@ -0,0 +1,26 @@
+package com.smart.helper;
+
+public class Message {
+	
+	private String message;
+	private String status;
+	
+	public Message(String message, String status) {
+		super();
+		this.message = message;
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
