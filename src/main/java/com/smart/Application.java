8d5c85aa278966df3430ee7c437415545e932302
Suhail <suhailmfos@gmail.com>
2025-01-06 01:39:07 +0530

    rest template

--- src/main/java/com/smart/Application.java
+++ src/main/java/com/smart/Application.java
@@ -1,11 +1,24 @@
 package com.smart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
